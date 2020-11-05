530e393cb08f55bb49f669f5ec8493935575baed
Ryan De Coster <50038256+Rdecoster@users.noreply.github.com>
2020-11-05 11:15:59 -0800

    Delete config.js

--- config/config.js
+++
@@ -1,8 +0,0 @@
-
-// const API_Key= '84692e19583a4dc6a7d4e747e2c3388b'
-const API_Key= 'fc4ccea1255040259120e72eb1f4b086'
-
-//other account fc4ccea1255040259120e72eb1f4b086
-module.exports = {
-API_Key,
-};
